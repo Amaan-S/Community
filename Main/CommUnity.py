f8d34fa0fd0b2ca7e70e9d5e08e67f0314fa3caa
Amaan-S <146875632+Amaan-S@users.noreply.github.com>
2024-12-07 22:54:20 -0600

    Update CommUnity.py
    changed flask port from 1098 to 5000 (the universal port)

--- Main/CommUnity.py
+++ Main/CommUnity.py
@@ -139,4 +139,4 @@ def organizer_home():
     return render_template('OrganizerHome.html', user=user)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=1098, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
